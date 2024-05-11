bb82360eae8417a29036ba220429f0b8dc2a1aac
Alex Guadalupe <9035@holbertonstudents.com>
2024-05-10 21:36:35 -0400

    task4 fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import os
-import imp
 
-module_path = '/tmp/hidden_4.pyc'
+if __name__ == "__main__":
 
-module = imp.load_compiled('module', module_path)
-
-for name in dir(module):
-    if not name.startswith('__'):
-        print(name)
-
-if __name__ == '__main__':
-    main()
+    from sys import argv
+    length = len(argv)
+    if length == 1:
+        print("0 arguments.")
+    elif length == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(length - 1))
+    for i in range(1, length):
+        print("{}: {}".format(i, argv[i]))
