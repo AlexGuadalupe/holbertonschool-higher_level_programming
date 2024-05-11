62b6e00a7dfdc0356ad598bea2b5a059a707be49
Alex Guadalupe <9035@holbertonstudents.com>
2024-05-10 21:16:13 -0400

    fix code task0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+from add_0 import add
+a = 1
+b = 2
+
+
+result = add(a, b)
+print('{} + {} = {}'.format(a, b, result))
+
 if __name__ == "__main__":
-    from add_0 import add
-    # * asign values to a and b
-    a = 1
-    b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+    main()
