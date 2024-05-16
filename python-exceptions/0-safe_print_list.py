b5c709991f99866b9e4eee8508b572c2afa5337b
Alex Guadalupe <9035@holbertonstudents.com>
2024-05-15 20:45:43 -0400

    task0 fix code

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(x):
-        try:
-            print(my_list[i])
-        except:
-            pass
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+        return x
+    except IndexError:
+        print()
         return i
