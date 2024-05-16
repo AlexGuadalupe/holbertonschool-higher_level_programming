4a125a18fe9bb2067c23e01b4a67947e19b89a2d
Alex Guadalupe <9035@holbertonstudents.com>
2024-05-16 15:41:54 -0400

    task 0a

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,7 @@
 class Square:
-    def __init__(self):
-        pass
+    def __init__(self, size=0):
+        self.__size = size
 
     @property
-    def area(self):
-
-        return self.__size**2
+    def size(self):
+        return self.__size
