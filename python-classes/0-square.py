a541a7e027b5f6af77a45ce3e19526273fe64a32
Alex Guadalupe <9035@holbertonstudents.com>
2024-05-16 15:43:40 -0400

    task0b

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,3 +5,14 @@ class Square:
     @property
     def size(self):
         return self.__size
+
+    def draw(self):
+        for _ in range(self.__size):
+            print('*' * self.__size)
+
+
+# Crear una instancia de Square con tamaño 5
+square = Square(5)
+
+# Dibujar el cuadrado
+square.draw()
