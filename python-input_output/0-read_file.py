8e65c56f15ce48e36894ccc7586afde6122f3643
Alex Guadalupe <9035@holbertonstudents.com>
2024-05-28 16:53:42 -0400

    task0 adding documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
+#!/usr/bin/python3
+"""A function that writes a string to a text file and returns the number of characters written."""
+
+
 def read_file(filename=""):
+    """A function that reads a text file (UTF8) and prints it to stdout."""
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         print(content)
         file.close()
-        return read_file
+    num_characters = len(content)
+    return num_characters
