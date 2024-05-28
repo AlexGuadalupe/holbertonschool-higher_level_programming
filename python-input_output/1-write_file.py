101151d2b35663d9c47492397cffa5a0ac7de2c7
Alex Guadalupe <9035@holbertonstudents.com>
2024-05-28 17:48:40 -0400

    task1 fix code

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,6 @@ of characters written."""
 def write_file(filename="", text=""):
     """A function that writes a string to a text file and returns the number
     of characters written."""
-    with open(filename) as file:
-        return file.write(text)
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+    return len(text)
