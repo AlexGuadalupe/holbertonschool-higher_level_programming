b1ac6726f2bd5172d50f1e28c181078fb57a6d5c
Alex Guadalupe <9035@holbertonstudents.com>
2024-06-17 22:03:41 -0400

    task8

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Un script que muestra el número de registros con id = 89 en la tabla first_table de la base de datos hbtn_0c_0 en tu servidor MySQL.
+SELECT COUNT(*) FROM first_table WHERE id = 89;
