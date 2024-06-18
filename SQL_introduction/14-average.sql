7a6a49173dcb8df421372f61909dae08d8fd2ecd
Alex Guadalupe <9035@holbertonstudents.com>
2024-06-17 22:44:45 -0400

    task 14

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- Un script que calcula el promedio de score de todos los registros en la tabla second_table de la base de datos hbtn_0c_0 en tu servidor MySQL.
+SELECT AVG(score) AS average FROM second_table;
