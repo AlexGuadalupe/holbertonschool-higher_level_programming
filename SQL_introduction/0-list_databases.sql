f11ca7e1860d3f9160aade4934ee50258a3da450
Alex Guadalupe <9035@holbertonstudents.com>
2024-06-17 20:18:16 -0400

    show databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,3 +1,13 @@
---SHOW DATABASE
+-- Script to list all databases on the MySQL server
 
+/*
+    This script connects to the MySQL server and lists all the databases.
+    Usage:
+    1. Connect to your MySQL server from the terminal or command line.
+    2. Execute this script using the source command or redirect it to the MySQL client.
+*/
+
+-- Show all databases available on the MySQL server
 SHOW DATABASES;
+
+-- End of script
