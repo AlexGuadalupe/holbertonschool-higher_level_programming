7fce74327983e0c3379da8b8204dd62bda9cd9ef
Alex Guadalupe <9035@holbertonstudents.com>
2024-06-17 22:23:21 -0400

    task11

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- Un script que lista todos los registros con un score >= 10 en la tabla second_table de la base de datos hbtn_0c_0 en tu servidor MySQL.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
