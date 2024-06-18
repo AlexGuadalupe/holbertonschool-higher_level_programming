cead82b3adad3c73e281399e61a05fb7dba74dac
Alex Guadalupe <9035@holbertonstudents.com>
2024-06-17 21:43:04 -0400

    task4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Un script que crea una tabla llamada first_table en la base de datos actual en tu servidor MySQL.
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
