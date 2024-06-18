7e840d1f23b02415e3a63107c30e8650f0cee7dd
Alex Guadalupe <9035@holbertonstudents.com>
2024-06-17 22:39:25 -0400

    task 13

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- Un script que elimina todos los registros con un score <= 5 en la tabla second_table de la base de datos hbtn_0c_0 en tu servidor MySQL.
+DELETE FROM second_table WHERE score <= 5;
