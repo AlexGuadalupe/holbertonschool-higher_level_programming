7063f0088501367ea2a37d6671833be1155518cc
Alex Guadalupe <9035@holbertonstudents.com>
2024-06-17 22:49:55 -0400

    task16gp

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- Un script que lista todos los registros de la tabla second_table de la base de datos hbtn_0c_0 en tu servidor MySQL.
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
