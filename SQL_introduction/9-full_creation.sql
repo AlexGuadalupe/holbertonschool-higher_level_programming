4a416ad250ac7de69c2d2ec1ef6b20967feee574
Alex Guadalupe <9035@holbertonstudents.com>
2024-06-17 22:11:35 -0400

    task9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Un script que crea una tabla llamada second_table en la base de datos hbtn_0c_0 en tu servidor MySQL y agrega múltiples filas.
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
